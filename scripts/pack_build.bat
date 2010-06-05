d2aa8716b3f3682220ca1d3828b9fcf0daefd5b7
Ivan Kravar??an <subchannel13@gmail.com>
2010-06-05 09:51:44 +0000

    New scripts for release automatization.
    Some more pictures of planets.

---
+++ scripts/pack_build.bat
@@ -0,0 +1,9 @@
+@echo off
+rem Runs copy_game_to_build.bat script and then
+rem copies game data from {svnRoot}\build\ to 
+rem {svnRoot}\release\Zvjezdojedac
+
+call copy_game_to_build.bat
+
+rmdir /s /q ..\release\Zvjezdojedac
+robocopy ../build/ ../release/Zvjezdojedac/ /xd .svn /e
