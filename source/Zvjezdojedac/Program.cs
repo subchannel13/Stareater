93d55ec92f6312e737fadb0c150ab5e2e6ba6c29
Ivan Vu?ica <ivan@vucica.net>
2011-03-01 22:29:47 +0100

    GtkFormMain added

--- source/Zvjezdojedac/Program.cs
+++ source/Zvjezdojedac/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+#if __MonoCS__
+using Gtk;
+#endif
 
 namespace Prototip
 {
@@ -13,9 +16,25 @@ namespace Prototip
 		[STAThread]
 		static void Main()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new FormMain());
+			bool runningOnMono = Type.GetType ("Mono.Runtime") != null;
+			if(!runningOnMono)
+			{
+				System.Windows.Forms.Application.EnableVisualStyles();
+				System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+				System.Windows.Forms.Application.Run(new FormMain());
+			}
+#if __MonoCS__
+			else
+			{
+				Gtk.Application.Init();
+				
+				GtkFormMain mainForm = new GtkFormMain();
+				mainForm.Show();
+	//			mainForm.ShowAll();
+				
+				Gtk.Application.Run();
+			}
+#endif
 		}
 	}
 }
