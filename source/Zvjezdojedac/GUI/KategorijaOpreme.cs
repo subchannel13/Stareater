b05c663f67c12ddf166d56a2b3046f2063b23d10
Ivan Krava??an <subchannel13@gmail.com>
2011-03-15 00:44:04 +0100

    Some more filename patterns to ignore list and some files I forgot to add to version control.

---
+++ source/Zvjezdojedac/GUI/KategorijaOpreme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototip
+{
+	enum KategorijaOpreme
+	{
+		Trup,
+		Misija,
+		MZPogon,
+		Oklop,
+		Potisnici,
+		Reaktor,
+		Senzor,
+		Specijalno,
+		Stit
+	}
+}
