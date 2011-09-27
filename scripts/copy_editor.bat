934de54f00e0c8e61131e27bf0cccca01131ef58
ikrvarscan <ikrvarscan@NTH-HR-ZG-0016>
2011-09-27 15:30:30 +0200

    Turn processing diagram

--- scripts/copy_editor.bat
+++ scripts/copy_editor.bat
@@ -2,7 +2,7 @@
 rem Copies ZvjEdit.exe from {projDir}/bin/debug/
 rem or {projDir}/bin/release/ to {svnRoot}/editors/
 rem
-rem Used as post build action i Zvjezdojedac editori
+rem Used as post build action in Zvjezdojedac editori
 rem project.
 
 copy ZvjEdit.exe ..\..\..\..\editors\
