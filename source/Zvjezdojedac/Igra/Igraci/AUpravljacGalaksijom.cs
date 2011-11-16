77f346651811745a7f1af0db010390a734283944
Ivan Kravar??an <Ivan Kravar??an@localhost>
2011-11-16 23:35:24 +0100

    Started implementation of AI.

---
+++ source/Zvjezdojedac/Igra/Igraci/AUpravljacGalaksijom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zvjezdojedac.Igra.Igraci
+{
+	abstract class AUpravljacGalaksijom
+	{
+		protected ARacunalniIgrac koordinator;
+
+		public AUpravljacGalaksijom(ARacunalniIgrac koordinator)
+		{
+			this.koordinator = koordinator;
+		}
+
+		public abstract void OdrediZahtjeve();
+		public abstract void Djeluj();
+	}
+}
