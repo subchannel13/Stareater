b90bbefc674ac4e431ac32701b9de7c490aa68b6
Ivan Kravar??an <Ivan Kravar??an@localhost>
2011-12-14 16:58:50 +0100

    More work on ship design GUI.

--- source/Zvjezdojedac/GUI/FormDizajnMisija.Designer.cs
+++ source/Zvjezdojedac/GUI/FormDizajnMisija.Designer.cs
@@ -27,41 +27,39 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.listView1 = new System.Windows.Forms.ListView();
-			this.header = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.flowPanel = new System.Windows.Forms.FlowLayoutPanel();
 			this.SuspendLayout();
 			// 
-			// listView1
+			// flowPanel
 			// 
-			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.header});
-			this.listView1.GridLines = true;
-			this.listView1.Location = new System.Drawing.Point(12, 12);
-			this.listView1.Name = "listView1";
-			this.listView1.Size = new System.Drawing.Size(121, 97);
-			this.listView1.TabIndex = 0;
-			this.listView1.UseCompatibleStateImageBehavior = false;
-			this.listView1.View = System.Windows.Forms.View.Details;
+			this.flowPanel.AutoScroll = true;
+			this.flowPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowPanel.Location = new System.Drawing.Point(0, 0);
+			this.flowPanel.Name = "flowPanel";
+			this.flowPanel.Size = new System.Drawing.Size(284, 262);
+			this.flowPanel.TabIndex = 0;
 			// 
 			// FormDizajnMisija
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 262);
-			this.Controls.Add(this.listView1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.ControlBox = false;
+			this.Controls.Add(this.flowPanel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.MaximizeBox = false;
 			this.Name = "FormDizajnMisija";
 			this.ShowIcon = false;
 			this.ShowInTaskbar = false;
-			this.Text = "FormDizajnMisija";
+			this.Text = "Select mission";
+			this.Load += new System.EventHandler(this.FormDizajnMisija_Load);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.ListView listView1;
-		private System.Windows.Forms.ColumnHeader header;
+		private System.Windows.Forms.FlowLayoutPanel flowPanel;
+
 	}
 }
--- source/Zvjezdojedac/GUI/FormDizajnMisija.cs
+++ source/Zvjezdojedac/GUI/FormDizajnMisija.cs
@@ -12,9 +12,13 @@ namespace Zvjezdojedac.GUI
 {
 	public partial class FormDizajnMisija : Form
 	{
-		Dictionary<Misija.Tip, List<Oruzje>> misije;
-
 		public Oruzje OdabranaMisija { get; private set; }
+
+		private const int VelicinaSlike = 80;
+		private const int BrRedaka = 3;
+		private const int BrStupaca = 3;
+		private Dictionary<Misija.Tip, List<Oruzje>> misije;
+		private Button pomocniGumb = new Button();
 
 		public FormDizajnMisija(Dictionary<Misija.Tip, List<Oruzje>> misije, Oruzje trenutnaMisija)
 		{
@@ -22,6 +26,44 @@ namespace Zvjezdojedac.GUI
 			this.OdabranaMisija = trenutnaMisija;
 
 			InitializeComponent();
+
+			this.ClientSize = new Size(
+				ukupnaDuljina(BrStupaca, VelicinaSlike + pomocniGumb.Margin.Horizontal, flowPanel.Margin.Horizontal) + SystemInformation.VerticalScrollBarWidth,
+				ukupnaDuljina(BrRedaka, VelicinaSlike + pomocniGumb.Margin.Vertical, flowPanel.Margin.Vertical)
+				);
+		}
+
+		private void FormDizajnMisija_Load(object sender, EventArgs e)
+		{
+			for (int misijaId = 0; misijaId < (int)Misija.Tip.N; misijaId++) {
+				Misija.Tip misijaTip = (Misija.Tip)misijaId;
+				if (!misije.ContainsKey(misijaTip))
+					continue;
+
+				foreach (Oruzje oruzje in misije[misijaTip])
+					flowPanel.Controls.Add(napraviGumb(oruzje));
+			}
+		}
+
+		private int ukupnaDuljina(int brKomponenti, int sirinaKomponente, int margine)
+		{
+			return brKomponenti * (sirinaKomponente + margine) + margine;
+		}
+
+		private Control napraviGumb(Oruzje oruzje)
+		{
+			Button gumb = new Button();
+
+			gumb.Size = new Size(VelicinaSlike + pomocniGumb.Margin.Horizontal, VelicinaSlike + pomocniGumb.Margin.Vertical);
+			gumb.BackgroundImage = oruzje.slika;
+			gumb.BackgroundImageLayout = ImageLayout.Zoom;
+			gumb.ForeColor = Color.DarkGray;
+			gumb.Text = oruzje.naziv;
+			gumb.TextAlign = ContentAlignment.BottomCenter;
+
+			gumb.Tag = oruzje;
+
+			return gumb;
 		}
 	}
 }
