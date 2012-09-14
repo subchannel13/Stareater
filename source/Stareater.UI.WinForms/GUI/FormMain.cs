e834bec4ddbe76f4a292ab04f829fa071ce4126e
Ivan Kravar??an <Ivan Kravar??an@localhost>
2012-09-15 01:25:47 +0200

    New project layout

---
+++ source/Stareater.UI.WinForms/GUI/FormMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Stareater.GUI
+{
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+		private void FormMain_Shown(object sender, EventArgs e)
+		{
+			using (FormMainMenu mainMenu = new FormMainMenu())
+				mainMenu.ShowDialog();
+		}
+	}
+}
