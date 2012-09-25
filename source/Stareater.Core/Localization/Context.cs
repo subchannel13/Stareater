122fbda3b33376716b9b0befc16c94a325cc89a2
Ivan Kravar??an <Ivan Kravar??an@localhost>
2012-09-26 00:02:38 +0200

    Started work on settings and localization

---
+++ source/Stareater.Core/Localization/Context.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using IKON;
+
+namespace Stareater.Localization
+{
+	public class Context : Value
+	{
+		private string name;
+		private Dictionary<string, Value> entries = new Dictionary<string, Value>();
+
+		internal Context(string name)
+		{
+			this.name = name;
+		}
+
+		public Value this[string memberName]
+		{
+			get { return entries[memberName]; }
+			internal set
+			{
+				if (entries.ContainsKey(memberName))
+					entries[memberName] = value;
+				else
+					entries.Add(memberName, value);
+			}
+		}
+
+		public override void Compose(Composer composer)
+		{
+			//NoOP
+		}
+
+		public override string TypeName
+		{
+			get { return "Context"; }
+		}
+	}
+}
--- source/Stareater.Core/Localization/Language.cs
+++ source/Stareater.Core/Localization/Language.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Stareater.Localization
 {
 	public class Language
 	{
+		Dictionary<string, Context> contexts = new Dictionary<string, Context>();
+
+		public string Code { get; private set; }
+
+		public Language(string code)
+		{
+
+		}
 	}
 }
