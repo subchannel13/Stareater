595377a824240ef6eaf053fb858e062075e2aacf
Ivan Kravar??an <Ivan Kravar??an@localhost>
2013-04-10 23:23:24 +0200

    Parser tester and game controller - Created a class for merging parse and evaluation tests into a single test unit - Rewrote tests - Created a game controller

---
+++ source/ExpressionParser Tests/ParserTester.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stareater.AppData.Expressions;
+
+namespace ExpressionParser_Tests
+{
+	class ParserTester
+	{
+		ExpressionParser parser;
+		double expectedOutput;
+		Dictionary<string, double> variables;
+		double delta = 0;
+
+		public ParserTester(string input, Dictionary<string, double> variables, double expectedOutput)
+		{
+			this.parser = new ExpressionParser(input);
+			parser.Parse();
+
+			this.expectedOutput = expectedOutput;
+			this.variables = variables;
+		}
+
+		public ParserTester(string input, Dictionary<string, double> variables, double expectedOutput, double delta)
+			: this(input, variables, expectedOutput)
+		{
+			this.delta = delta;
+		}
+
+		public bool IsOK
+		{
+			get
+			{
+				if (parser.errors.count > 0)
+					return false;
+
+				double evaluated = parser.ParsedFormula.Evaluate(variables);
+				
+				return Math.Abs(evaluated - expectedOutput) <= delta ||
+					(double.IsNaN(evaluated) && double.IsNaN(expectedOutput)) ||
+					(double.IsNegativeInfinity(evaluated) && double.IsNegativeInfinity(expectedOutput)) ||
+					(double.IsPositiveInfinity(evaluated) && double.IsPositiveInfinity(expectedOutput))
+					;				
+			}
+		}
+
+		public string Message
+		{
+			get
+			{
+				if (parser.errors.count != 0)
+					return parser.errors.errorMessages.ToString();
+				
+				return "Expected: " + expectedOutput + Environment.NewLine + "Evaluated: " + parser.ParsedFormula.Evaluate(variables);
+			}
+		}
+	}
+}
---
+++ source/Stareater.Core/Controllers/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stareater.Controllers.Data;
+
+namespace Stareater.Controllers
+{
+	public class GameController
+	{
+		public GameState State { get; private set; }
+
+		public GameController()
+		{
+			State = GameState.NoGame;
+		}
+	}
+}
