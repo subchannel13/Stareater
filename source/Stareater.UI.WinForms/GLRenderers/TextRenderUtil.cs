4a113ca74b95ce23341ff9845a048c3bdfb95467
Ivan Kravar??an <Ivan Kravar??an@localhost>
2013-06-21 00:11:36 +0200

    Star names rendering

--- source/Stareater.UI.WinForms/GLRenderers/TextRenderUtil.cs
+++ source/Stareater.UI.WinForms/GLRenderers/TextRenderUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using OpenTK;
+using OpenTK.Graphics.OpenGL;
 
 namespace Stareater.GLRenderers
 {
@@ -28,18 +29,27 @@ namespace Stareater.GLRenderers
 		
 		Bitmap textureBitmap;
 		Font font;
-		Dictionary<char, CharTextureInfo> characterPositions = new Dictionary<char, CharTextureInfo>();
 		Vector2 nextCharOffset;
+
+		Dictionary<char, CharTextureInfo> characterInfos = new Dictionary<char, CharTextureInfo>();
+		Vector2[] unitQuad;
 		
 		private TextRenderUtil()
-		{ }
+		{
+			unitQuad = new Vector2[] {
+				new Vector2(0, 0),
+				new Vector2(0, -1),
+				new Vector2(1, -1),
+				new Vector2(1, 0),
+			};
+		}
 
 		public void Prepare(IEnumerable<string> texts)
 		{
 			HashSet<char> missinCharacters = new HashSet<char>();
 			foreach (string text in texts)
 				foreach (char c in text)
-					if (!characterPositions.ContainsKey(c))
+					if (!characterInfos.ContainsKey(c))
 						missinCharacters.Add(c);
 
 			if (missinCharacters.Count == 0 && TextureManager.Get.FontTextureId == 0)
@@ -64,12 +74,14 @@ namespace Stareater.GLRenderers
 					if (nextCharOffset.X + size.Width >= 1)
 						nextCharOffset += new Vector2(0, size.Height);
 
-					characterPositions.Add(c, new CharTextureInfo(nextCharOffset, size));
-					g.DrawString(c.ToString(), font, textBrush, nextCharOffset.X, nextCharOffset.Y, StringFormat.GenericTypographic);
+					characterInfos.Add(c, new CharTextureInfo(nextCharOffset, size));
+					g.DrawString(c.ToString(), font, textBrush, nextCharOffset.X * Width, nextCharOffset.Y * Height, StringFormat.GenericTypographic);
 
 					nextCharOffset += new Vector2(size.Width, 0);
 				}
 			}
+
+			TextureManager.Get.UpdateTexture(TextureManager.Get.FontTextureId, textureBitmap);
 		}
 		
 		private void lazyInitialization()
@@ -82,6 +94,32 @@ namespace Stareater.GLRenderers
 			
 			if (TextureManager.Get.FontTextureId == 0)
 				TextureManager.Get.Load(TextureContext.Font, textureBitmap);
+		}
+
+		public void RenderText(string text, float adjustment)
+		{
+			float textWidth = 0;
+			foreach (char c in text)
+				textWidth += characterInfos[c].Aspect;
+
+			GL.BindTexture(TextureTarget.Texture2D, TextureManager.Get.FontTextureId);
+
+			float charOffset = textWidth * adjustment;
+			GL.Begin(BeginMode.Quads);
+
+			foreach (char c in text) {
+				var charInfo = characterInfos[c];
+				
+				for (int v = 0; v < 4; v++) {
+					GL.TexCoord2(charInfo.TextureCoords[v]);
+					GL.Vertex2(
+						unitQuad[v].X * charInfo.Aspect + charOffset,
+						unitQuad[v].Y
+					);
+				}
+				charOffset += charInfo.Aspect;
+			}
+			GL.End();
 		}
 	}
 }
