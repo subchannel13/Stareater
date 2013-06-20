5838f6afaf9e0bd455f16e42e0e7cc8d9ef63284
Ivan Kravar??an <Ivan Kravar??an@localhost>
2013-06-20 20:48:34 +0200

    Character texture preparation

---
+++ source/Stareater.UI.WinForms/GLRenderers/CharTextureInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+
+namespace Stareater.GLRenderers
+{
+	public class CharTextureInfo
+	{
+		public Vector2[] TextureCoords { get; private set; }
+		public float Aspect { get; private set; }
+		
+		public CharTextureInfo(Vector2 offset, SizeF size)
+		{
+			this.TextureCoords = new Vector2[] {
+				offset,
+				offset + new Vector2(0, size.Height),
+				offset + new Vector2(size.Width, size.Height),
+				offset + new Vector2(size.Width, 0),
+			};
+			
+			this.Aspect = size.Width / size.Height;
+		}
+	}
+}
--- source/Stareater.UI.WinForms/GLRenderers/TextRenderUtil.cs
+++ source/Stareater.UI.WinForms/GLRenderers/TextRenderUtil.cs
@@ -21,25 +21,67 @@ namespace Stareater.GLRenderers
 		}
 		#endregion
 		
-		Dictionary<char, Vector2[]> characterPositions = new Dictionary<char, Vector2[]>();
+		const int Width = 512;
+		const int Height = 512;
+		const float FontSize = 30;	
+		const string FontFamily = "Arial";
+		
+		Bitmap textureBitmap;
+		Font font;
+		Dictionary<char, CharTextureInfo> characterPositions = new Dictionary<char, CharTextureInfo>();
+		Vector2 nextCharOffset;
 		
 		private TextRenderUtil()
 		{ }
-		
+
 		public void Prepare(IEnumerable<string> texts)
-		{ 
+		{
 			HashSet<char> missinCharacters = new HashSet<char>();
-			foreach(string text in texts)
-				foreach(char c in text)
+			foreach (string text in texts)
+				foreach (char c in text)
 					if (!characterPositions.ContainsKey(c))
-						missinCharacters.Add(c);					
-				
-			Bitmap textureBitmap;
-			
-			if (TextureManager.Get.FontTextureId == 0) {
-				textureBitmap = new Bitmap(512, 512);
-				TextureManager.Get.Load(TextureContext.Font, textureBitmap);
+						missinCharacters.Add(c);
+
+			if (missinCharacters.Count == 0 && TextureManager.Get.FontTextureId == 0)
+				return;
+
+			lazyInitialization();
+
+			using (Graphics g = Graphics.FromImage(textureBitmap)) {
+				if (nextCharOffset == Vector2.Zero)
+					g.Clear(Color.Transparent);
+
+				g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+				Brush textBrush = new SolidBrush(Color.White);
+
+				foreach (char c in missinCharacters) {
+					SizeF size = g.MeasureString(c.ToString(), font, int.MaxValue, StringFormat.GenericTypographic);
+					size = new SizeF(
+						(float)Math.Ceiling(size.Width) / Width,
+						(float)Math.Ceiling(size.Height) / Height
+					);
+
+					if (nextCharOffset.X + size.Width >= 1)
+						nextCharOffset += new Vector2(0, size.Height);
+
+					characterPositions.Add(c, new CharTextureInfo(nextCharOffset, size));
+					g.DrawString(c.ToString(), font, textBrush, nextCharOffset.X, nextCharOffset.Y, StringFormat.GenericTypographic);
+
+					nextCharOffset += new Vector2(size.Width, 0);
+				}
 			}
+		}
+		
+		private void lazyInitialization()
+		{
+			if (textureBitmap == null)
+				textureBitmap = new Bitmap(Width, Height);
+			
+			if (font == null)
+				font = new Font(FontFamily, FontSize, FontStyle.Bold);
+			
+			if (TextureManager.Get.FontTextureId == 0)
+				TextureManager.Get.Load(TextureContext.Font, textureBitmap);
 		}
 	}
 }
