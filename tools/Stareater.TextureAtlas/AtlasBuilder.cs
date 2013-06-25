10c49493392af7ce8d0f656d8aa01cca0a185a98
Ivan Kravar??an <Ivan Kravar??an@localhost>
2013-06-25 16:13:53 +0200

    Texture atlas tool

---
+++ tools/Stareater.TextureAtlas/AtlasBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Stareater.TextureAtlas
+{
+	class AtlasBuilder
+	{
+		private KeyValuePair<string, Size>[] sizes;
+		private Size bucketSize;
+		private int margin;
+
+		public AtlasBuilder(KeyValuePair<string, Size>[] sizes, int margin, Size bucketSize)
+		{
+			this.bucketSize = bucketSize;
+			this.sizes = sizes;
+			this.margin = margin;
+
+			Size marginPoint = new Size(2 * margin, 2 * margin);
+			for (int i = 0; i < sizes.Length; i++)
+				this.sizes[i] = new KeyValuePair<string, Size>(this.sizes[i].Key, sizes[i].Value + marginPoint);
+		}
+
+		public IEnumerable<KeyValuePair<string, Rectangle>> Build()
+		{
+			PartitionNode bucket = new PartitionNode(bucketSize);
+
+			Point? itemPosition;
+			for (int i = 0; i < sizes.Length; i++) {
+				itemPosition = bucket.Add(i, sizes[i].Value);
+				if (itemPosition == null)
+					throw new Exception("Bucket is too small");
+
+				yield return new KeyValuePair<string, Rectangle>(
+					sizes[i].Key,
+					new Rectangle(
+						itemPosition.Value + new Size(margin, margin),
+						sizes[i].Value - new Size(2 * margin, 2 * margin)
+					)
+				);
+			}
+		}
+	}
+}
---
+++ tools/Stareater.TextureAtlas/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing.Imaging;
+using Ikadn.Ikon.Types;
+using System.Drawing;
+using Ikadn;
+
+namespace Stareater.TextureAtlas
+{
+	class Program
+	{
+		static Dictionary<string, Bitmap> items = new Dictionary<string, Bitmap>();
+		static Dictionary<string, FileInfo> files = new Dictionary<string, FileInfo>();
+		static int margin = 4;
+		static int width = 512;
+		static int heigth = 512;
+		static string outputIkonPath = "output.txt";
+		static string outputImagePath = "output.png";
+		static string compositeTag = "TextureAtlas";
+
+		static void Main(string[] args)
+		{
+			if (args.Length < 1) {
+				Console.WriteLine("Usage: TextureAtlas <folder> [options]");
+				return;
+			} else
+				ReadParameters(args);
+
+			AtlasBuilder builder = new AtlasBuilder(
+				items.Select(x => new KeyValuePair<string, Size>(x.Key, x.Value.Size)).ToArray(), 
+				margin, new Size(width, heigth)
+			);
+
+			IkonComposite atlasIkon = new IkonComposite(compositeTag);
+			Bitmap atlasImage = new Bitmap(width, heigth);
+
+			using(Graphics g = Graphics.FromImage(atlasImage))
+				foreach (var x in builder.Build()) {
+					Console.WriteLine(x.Key + " " + x.Value);
+
+					IkonArray textureCoords = new IkonArray();
+					textureCoords.Add(serializeRectangle(x.Value.Left, x.Value.Top));
+					textureCoords.Add(serializeRectangle(x.Value.Left, x.Value.Bottom));
+					textureCoords.Add(serializeRectangle(x.Value.Right, x.Value.Bottom));
+					textureCoords.Add(serializeRectangle(x.Value.Right, x.Value.Top));
+
+					atlasIkon.Add(x.Key, textureCoords);
+					g.DrawImage(items[x.Key], x.Value.Location);
+				}
+
+			using (var outStream = new StreamWriter(outputIkonPath)) {
+				IkadnWriter writer = new IkadnWriter(outStream);
+				atlasIkon.Compose(writer);
+			}
+
+			atlasImage.Save(outputImagePath, ImageFormat.Png);
+		}
+
+		private static Ikadn.IkadnBaseObject serializeRectangle(float x, float y)
+		{
+			IkonArray result = new IkonArray();
+			result.Add(new IkonNumeric(x / width));
+			result.Add(new IkonNumeric(y / heigth));
+
+			return result;
+		}
+		
+		static void ReadParameters(string[] args)
+		{
+			Queue<string> parameters = new Queue<string>(args);
+
+			foreach (var file in new DirectoryInfo(parameters.Dequeue()).GetFiles()) {
+				if (file.Extension.ToLower() != ".png" && file.Extension.ToLower() != ".bmp" && file.Extension.ToLower() != ".jpg")
+					continue;
+
+				string name = Path.GetFileNameWithoutExtension(file.Name);
+				items.Add(name, new Bitmap(file.FullName));
+				files.Add(name, file);
+			}
+
+			margin = 8;
+		}
+	}
+}
