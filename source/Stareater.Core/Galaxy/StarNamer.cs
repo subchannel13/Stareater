531e7c59ffb3b80893030d85bcf83a8df81e761a
Ivan Kravar??an <Ivan Kravar??an@localhost>
2013-06-28 23:18:55 +0200

    Star name data containers

---
+++ source/Stareater.Core/Galaxy/StarNamer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Stareater.Localization.StarNames;
+
+namespace Stareater.Galaxy
+{
+	public class StarNamer
+	{
+		Queue<IStarName> starNames = new Queue<IStarName>();
+		
+		public StarNamer(int starCount)
+		{
+			Settings
+		}
+		
+		public IStarName NextName()
+		{
+			return starNames.Dequeue();
+		}
+	}
+}
---
+++ source/Stareater.Core/Localization/StarNames/ConstellationStarName.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Stareater.Localization.StarNames
+{
+	public class ConstellationStarName : IStarName
+	{
+		const string ConstellationsContext = "ConstellationNames";
+		const string ConstellationKeyPrefix = "constellation";
+		const string NominativeSufix = "Nom";
+		const string GenitiveSufix = "Gen";
+		const string DesignationContext = "starDesignations";
+		const string DesignationKeyPrefix = "designation";
+		
+		const int NoDesignation = -1;
+		
+		int constellation;
+		int designation;
+		
+		public ConstellationStarName(int constellationNameIndex, int designationIndex)
+		{
+			this.constellation = constellationNameIndex;
+			this.designation = designationIndex;
+		}
+		
+		public ConstellationStarName(int constellationNameIndex)
+		{
+			this.constellation = constellationNameIndex;
+			this.designation = NoDesignation;
+		}
+		
+		public string Name(Language language)
+		{
+			if (designation == NoDesignation)
+				return language[ConstellationsContext][ConstellationKeyPrefix + constellation.ToString() + NominativeSufix].Text();
+			else
+				return language[DesignationContext][DesignationKeyPrefix + designation.ToString()].Text() +
+					" " +
+					language[ConstellationsContext][ConstellationKeyPrefix + constellation.ToString() + GenitiveSufix].Text();
+		}
+	}
+}
