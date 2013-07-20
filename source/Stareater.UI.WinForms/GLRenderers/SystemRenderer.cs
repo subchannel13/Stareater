919ec4a4deeb49743ade6f39397011f78e22131a
Ivan Kravar??an <subchannel13@gmail.com>
2013-07-20 13:27:25 +0200

    Stub system renderer and switching between galaxy and system rendering

---
+++ source/Stareater.UI.WinForms/GLRenderers/SystemRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using Stareater.Controllers;
+
+namespace Stareater.GLRenderers
+{
+	public class SystemRenderer : IRenderer
+	{
+		private GameController controller;
+		private Control eventDispatcher;
+		private Action systemClosedHandler;
+		
+		public SystemRenderer(GameController controller, Action systemClosedHandler)
+		{
+			this.controller = controller;
+			this.systemClosedHandler = systemClosedHandler;
+		}
+		
+		public void Draw(double deltaTime)
+		{
+			//no op
+		}
+		
+		public void Load()
+		{
+			//no op
+		}
+		
+		public void Unload()
+		{
+			//no op
+		}
+		
+		public void AttachToCanvas(Control eventDispatcher)
+		{
+			this.eventDispatcher = eventDispatcher;
+			
+			eventDispatcher.MouseClick += mouseClick;
+		}
+		
+		public void DetachFromCanvas()
+		{
+			eventDispatcher.MouseClick -= mouseClick;
+			
+			this.eventDispatcher = null;
+		}
+		
+		private void mouseClick(object sender, MouseEventArgs e)
+		{
+			this.systemClosedHandler();
+		}
+		
+		public void Dispose()
+		{
+			if (eventDispatcher != null) {
+				DetachFromCanvas();
+				Unload();
+			}
+		}
+	}
+}
