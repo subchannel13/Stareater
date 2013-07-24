1dd310d3948a8a273f93746d6b9df5f0dfec9cea
Ivan Kravar??an <subchannel13@gmail.com>
2013-07-24 11:33:24 +0200

    System view mockup

---
+++ documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/SystemMockup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Winforms_Mockups
+{
+	/// <summary>
+	/// Description of SystemMockup.
+	/// </summary>
+	public partial class SystemMockup : UserControl
+	{
+		public SystemMockup()
+		{
+			InitializeComponent();
+		}
+		
+		void SystemMockupPaint(object sender, PaintEventArgs e)
+		{
+			e.Graphics.Clear(Color.Black);
+			
+			RectangleF centerRect = new RectangleF(-100, 50, 200, 200);
+			e.Graphics.FillEllipse(new SolidBrush(Color.Yellow), centerRect);
+			
+			RectangleF bodyRect = new RectangleF(75, 125, 50, 50);
+			bodyRect.Inflate(10, 10);
+			for(int i = 0; i < 3; i++)
+			{
+				centerRect.Inflate(100, 100);
+				e.Graphics.DrawEllipse(new Pen(Color.LimeGreen, 3), centerRect);
+				
+				bodyRect.Offset(100, 0);
+				e.Graphics.FillEllipse(new SolidBrush(
+					new Color[]{
+						Color.Peru,
+						Color.Blue,
+						Color.LightGray
+					}[i]), bodyRect);
+			}
+		}
+	}
+}
