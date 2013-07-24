0be117cbeb589775f6e278a2faddc71762547a92
Ivan Kravar??an <subchannel13@gmail.com>
2013-07-24 11:33:24 +0200

    System view mockup

--- documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/FormStarSystem.Designer.cs
+++ documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/FormStarSystem.Designer.cs
@@ -27,101 +27,27 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
-			this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
-			this.ovalShape2 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
-			this.ovalShape3 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
-			this.ovalShape4 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
-			this.ovalShape5 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.button1 = new System.Windows.Forms.Button();
-			this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+			this.managementPanel = new System.Windows.Forms.Panel();
 			this.label1 = new System.Windows.Forms.Label();
-			this.panel1.SuspendLayout();
+			this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+			this.button1 = new System.Windows.Forms.Button();
+			this.systemMockup1 = new Winforms_Mockups.SystemMockup();
+			this.managementPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// shapeContainer1
+			// managementPanel
 			// 
-			this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
-			this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
-			this.shapeContainer1.Name = "shapeContainer1";
-			this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
-            this.ovalShape5,
-            this.ovalShape4,
-            this.ovalShape3,
-            this.ovalShape2,
-            this.ovalShape1});
-			this.shapeContainer1.Size = new System.Drawing.Size(568, 401);
-			this.shapeContainer1.TabIndex = 0;
-			this.shapeContainer1.TabStop = false;
-			// 
-			// ovalShape1
-			// 
-			this.ovalShape1.FillColor = System.Drawing.Color.Yellow;
-			this.ovalShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
-			this.ovalShape1.Location = new System.Drawing.Point(-111, 11);
-			this.ovalShape1.Name = "ovalShape1";
-			this.ovalShape1.Size = new System.Drawing.Size(250, 250);
-			// 
-			// ovalShape2
-			// 
-			this.ovalShape2.BorderColor = System.Drawing.Color.Lime;
-			this.ovalShape2.Location = new System.Drawing.Point(-248, -128);
-			this.ovalShape2.Name = "ovalShape2";
-			this.ovalShape2.Size = new System.Drawing.Size(500, 500);
-			// 
-			// ovalShape3
-			// 
-			this.ovalShape3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-			this.ovalShape3.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
-			this.ovalShape3.Location = new System.Drawing.Point(214, 102);
-			this.ovalShape3.Name = "ovalShape3";
-			this.ovalShape3.Size = new System.Drawing.Size(80, 80);
-			// 
-			// ovalShape4
-			// 
-			this.ovalShape4.BorderColor = System.Drawing.Color.Lime;
-			this.ovalShape4.Location = new System.Drawing.Point(-374, -282);
-			this.ovalShape4.Name = "ovalShape4";
-			this.ovalShape4.Size = new System.Drawing.Size(800, 800);
-			// 
-			// ovalShape5
-			// 
-			this.ovalShape5.FillColor = System.Drawing.Color.Teal;
-			this.ovalShape5.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
-			this.ovalShape5.Location = new System.Drawing.Point(380, 104);
-			this.ovalShape5.Name = "ovalShape5";
-			this.ovalShape5.Size = new System.Drawing.Size(80, 80);
-			// 
-			// panel1
-			// 
-			this.panel1.BackColor = System.Drawing.SystemColors.Control;
-			this.panel1.BackgroundImage = global::Winforms_Mockups.Properties.Resources.metalic;
-			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.panel1.Controls.Add(this.label1);
-			this.panel1.Controls.Add(this.hScrollBar1);
-			this.panel1.Controls.Add(this.button1);
-			this.panel1.Location = new System.Drawing.Point(122, 296);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(316, 105);
-			this.panel1.TabIndex = 1;
-			// 
-			// button1
-			// 
-			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.button1.Location = new System.Drawing.Point(8, 11);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(80, 80);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// hScrollBar1
-			// 
-			this.hScrollBar1.Location = new System.Drawing.Point(90, 13);
-			this.hScrollBar1.Name = "hScrollBar1";
-			this.hScrollBar1.Size = new System.Drawing.Size(206, 17);
-			this.hScrollBar1.TabIndex = 1;
+			this.managementPanel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.managementPanel.BackColor = System.Drawing.SystemColors.Control;
+			this.managementPanel.BackgroundImage = global::Winforms_Mockups.Properties.Resources.metalic;
+			this.managementPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.managementPanel.Controls.Add(this.label1);
+			this.managementPanel.Controls.Add(this.hScrollBar1);
+			this.managementPanel.Controls.Add(this.button1);
+			this.managementPanel.Location = new System.Drawing.Point(126, 296);
+			this.managementPanel.Name = "managementPanel";
+			this.managementPanel.Size = new System.Drawing.Size(316, 105);
+			this.managementPanel.TabIndex = 1;
 			// 
 			// label1
 			// 
@@ -130,35 +56,55 @@
 			this.label1.ForeColor = System.Drawing.Color.White;
 			this.label1.Location = new System.Drawing.Point(87, 30);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.Size = new System.Drawing.Size(39, 13);
 			this.label1.TabIndex = 2;
-			this.label1.Text = "label1";
+			this.label1.Text = "7 turns";
+			// 
+			// hScrollBar1
+			// 
+			this.hScrollBar1.Location = new System.Drawing.Point(90, 13);
+			this.hScrollBar1.Name = "hScrollBar1";
+			this.hScrollBar1.Size = new System.Drawing.Size(206, 17);
+			this.hScrollBar1.TabIndex = 1;
+			this.hScrollBar1.Value = 75;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button1.Image = global::Winforms_Mockups.Properties.Resources.small_cruiser;
+			this.button1.Location = new System.Drawing.Point(8, 11);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(80, 80);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// systemMockup1
+			// 
+			this.systemMockup1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.systemMockup1.Location = new System.Drawing.Point(0, 0);
+			this.systemMockup1.Name = "systemMockup1";
+			this.systemMockup1.Size = new System.Drawing.Size(568, 401);
+			this.systemMockup1.TabIndex = 2;
 			// 
 			// FormStarSystem
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.Black;
+			this.BackColor = System.Drawing.SystemColors.Control;
 			this.ClientSize = new System.Drawing.Size(568, 401);
-			this.Controls.Add(this.panel1);
-			this.Controls.Add(this.shapeContainer1);
+			this.Controls.Add(this.managementPanel);
+			this.Controls.Add(this.systemMockup1);
 			this.Name = "FormStarSystem";
 			this.Text = "FormStarSystem";
-			this.panel1.ResumeLayout(false);
-			this.panel1.PerformLayout();
+			this.managementPanel.ResumeLayout(false);
+			this.managementPanel.PerformLayout();
 			this.ResumeLayout(false);
-
 		}
+		private Winforms_Mockups.SystemMockup systemMockup1;
 
 		#endregion
 
-		private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
-		private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
-		private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape5;
-		private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape4;
-		private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape3;
-		private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape2;
-		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel managementPanel;
 		private System.Windows.Forms.HScrollBar hScrollBar1;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Label label1;
---
+++ documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/SystemMockup.Designer.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ekraiva
+ * Date: 22.7.2013.
+ * Time: 14:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Winforms_Mockups
+{
+	partial class SystemMockup
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the control.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// SystemMockup
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Name = "SystemMockup";
+			this.Size = new System.Drawing.Size(491, 380);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.SystemMockupPaint);
+			this.ResumeLayout(false);
+		}
+	}
+}
