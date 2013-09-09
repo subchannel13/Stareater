9cdf6de6d406f49b877b34f826c5e211f20bf7c2
Ivan Kravar??an <subchannel13@gmail.com>
2013-09-09 13:00:30 +0200

    Technology progress classes

---
+++ source/Stareater.Core/GameData/Databases/Tables/TechProgressCollection.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using Stareater.Players;
+using Stareater.Utils.Collections;
+
+namespace Stareater.GameData.Databases.Tables
+{
+	class TechProgressCollection : ICollection<TechnologyProgress>, IDelayedRemoval<TechnologyProgress>
+	{
+		HashSet<TechnologyProgress> innerSet = new HashSet<TechnologyProgress>();
+		List<TechnologyProgress> toRemove = new List<TechnologyProgress>();
+
+		Dictionary<Player, List<TechnologyProgress>> PlayersIndex = new Dictionary<Player, List<TechnologyProgress>>();
+
+		public IEnumerable<TechnologyProgress> Players(Player key) {
+			if (PlayersIndex.ContainsKey(key))
+				foreach (var item in PlayersIndex[key])
+					yield return item;
+		}
+	
+		public void Add(TechnologyProgress item)
+		{
+			innerSet.Add(item); 
+
+			if (!PlayersIndex.ContainsKey(item.Owner))
+				PlayersIndex.Add(item.Owner, new List<TechnologyProgress>());
+			PlayersIndex[item.Owner].Add(item);
+		}
+
+		public void Add(IEnumerable<TechnologyProgress> items)
+		{
+			foreach(var item in items)
+				Add(item);
+		}
+		
+		public void Clear()
+		{
+			innerSet.Clear();
+
+			PlayersIndex.Clear();
+		}
+
+		public bool Contains(TechnologyProgress item)
+		{
+			return innerSet.Contains(item);
+		}
+
+		public void CopyTo(TechnologyProgress[] array, int arrayIndex)
+		{
+			innerSet.CopyTo(array, arrayIndex);
+		}
+
+		public int Count
+		{
+			get { return innerSet.Count; }
+		}
+
+		public bool IsReadOnly
+		{
+			get { return false; }
+		}
+
+		public bool Remove(TechnologyProgress item)
+		{
+			if (innerSet.Remove(item)) {
+				PlayersIndex[item.Owner].Remove(item);
+			
+				return true;
+			}
+
+			return false;
+		}
+
+		public IEnumerator<TechnologyProgress> GetEnumerator()
+		{
+			return innerSet.GetEnumerator();
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return innerSet.GetEnumerator();
+		}
+
+		public void PendRemove(TechnologyProgress element)
+		{
+			toRemove.Add(element);
+		}
+
+		public void ApplyRemove()
+		{
+			foreach (var element in toRemove)
+				this.Remove(element);
+			toRemove.Clear();
+		}
+	}
+}
---
+++ source/Stareater.Core/GameData/TechnologyProgress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Stareater.Players;
+
+namespace Stareater.GameData
+{
+	public class TechnologyProgress
+	{
+		public const int NotStarted = -1;
+			
+		public int Level { get; private set; }
+		public double InvestedPoints { get; private set; }
+		public Technology Topic { get; private set; }
+		public Player Owner { get; private set; }
+		
+		public TechnologyProgress(int level, double investedPoints, Technology topic, Player owner)
+		{
+			this.Level = level;
+			this.InvestedPoints = investedPoints;
+			this.Topic = topic;
+			this.Owner = owner;
+		}
+		
+		public TechnologyProgress(Technology topic, Player owner) : 
+			this (NotStarted, 0, topic, owner)
+		{ }
+	}
+}
