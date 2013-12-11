d364f01c5161b1583f84bb745c0c9fbb122528f9
Ivan Kravar??an <Ivan Kravar??an@localhost>
2013-12-11 19:54:58 +0100

    Stellaris processor

---
+++ source/Stareater.Core/GameLogic/StellarisProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stareater.Galaxy;
+using Stareater.Players;
+using Stareater.GameData;
+
+namespace Stareater.GameLogic
+{
+	class StellarisProcessor
+	{
+		public StellarisAdmin Stellaris { get; set; }
+
+		public StellarisProcessor(StellarisAdmin stellaris)
+		{
+			this.Stellaris = stellaris;
+		}
+
+		public Player Owner
+		{
+			get
+			{
+				return Stellaris.Owner;
+			}
+		}
+
+		internal StellarisProcessor Copy(PlayersRemap playerRemap)
+		{
+			StellarisProcessor copy = new StellarisProcessor(playerRemap.Stellarises[this.Stellaris]);
+
+			return copy;
+		}
+	}
+}
