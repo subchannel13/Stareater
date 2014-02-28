1e2cdd68c06cf08f0c0eb0fed30dfd69029d8b7d
Ivan Kravar??an <subchannel13@gmail.com>
2014-02-28 16:12:14 +0100

    Ship design GUI mockup

--- documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/DesignItem.cs
+++ documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/DesignItem.cs
@@ -1,33 +1,15 @@
-﻿/*
- * Created by SharpDevelop.
- * User: ekraiva
- * Date: 14.2.2014.
- * Time: 9:50
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
 
 namespace Winforms_Mockups
 {
-	/// <summary>
-	/// Description of DesignItem.
-	/// </summary>
 	public partial class DesignItem : UserControl
 	{
 		public DesignItem()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
 			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
 		}
 	}
 }
--- documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/FormShipDesign.Designer.cs
+++ documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/FormShipDesign.Designer.cs
@@ -36,12 +36,162 @@ namespace Winforms_Mockups
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Winforms_Mockups.Properties.Resources.small_cruiser;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.BackgroundImage = global::Winforms_Mockups.Properties.Resources.arrowLeft;
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button1.Location = new System.Drawing.Point(12, 98);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(16, 16);
+			this.button1.TabIndex = 1;
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.BackgroundImage = global::Winforms_Mockups.Properties.Resources.arrowRight;
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button2.Location = new System.Drawing.Point(60, 98);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(16, 16);
+			this.button2.TabIndex = 2;
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(98, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(140, 20);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.Text = "Colonizer";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+									"Cruiser",
+									"Fighter"});
+			this.comboBox1.Location = new System.Drawing.Point(98, 38);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(140, 21);
+			this.comboBox1.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(48, 142);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(44, 13);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Primary:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(31, 178);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(61, 13);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "Secondary:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(48, 214);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(44, 13);
+			this.label5.TabIndex = 12;
+			this.label5.Text = "Shields:";
+			// 
+			// button6
+			// 
+			this.button6.Image = global::Winforms_Mockups.Properties.Resources.colonizer;
+			this.button6.Location = new System.Drawing.Point(98, 133);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(140, 30);
+			this.button6.TabIndex = 13;
+			this.button6.Text = "200 x Colonizer";
+			this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Image = global::Winforms_Mockups.Properties.Resources.laser;
+			this.button3.Location = new System.Drawing.Point(98, 169);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(140, 30);
+			this.button3.TabIndex = 14;
+			this.button3.Text = "1.5 k x Laser";
+			this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Image = global::Winforms_Mockups.Properties.Resources.radiation_shield;
+			this.button4.Location = new System.Drawing.Point(98, 205);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(140, 30);
+			this.button4.TabIndex = 15;
+			this.button4.Text = "EM shield";
+			this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+			this.button4.UseVisualStyleBackColor = true;
 			// 
 			// FormShipDesign
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "FormShipDesign";
+			this.ClientSize = new System.Drawing.Size(401, 321);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.Name = "FormShipDesign";
+			this.Text = "FormShipDesign";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
