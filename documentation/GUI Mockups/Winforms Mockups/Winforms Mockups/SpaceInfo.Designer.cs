e4ec1bdd103d796b57d5cbe3053e1515a325f1c9
Ivan Kravar??an <subchannel13@gmail.com>
2014-03-12 15:44:27 +0100

    Even more work on ship design GUI mockup

---
+++ documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/SpaceInfo.Designer.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ekraiva
+ * Date: 10.3.2014.
+ * Time: 15:46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Winforms_Mockups
+{
+	partial class SpaceInfo
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the control.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+									| System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(286, 79);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "140 / 200";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// SpaceInfo
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.label1);
+			this.Name = "SpaceInfo";
+			this.Size = new System.Drawing.Size(286, 79);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label1;
+	}
+}
