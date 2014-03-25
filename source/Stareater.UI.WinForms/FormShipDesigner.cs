500fdc8a353675a8be440d7300c9096c5609e851
Ivan Kravar??an <subchannel13@gmail.com>
2014-03-25 16:10:05 +0100

    More ship images and stub ship designer GUI

---
+++ source/Stareater.UI.WinForms/FormShipDesigner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Stareater
+{
+	public partial class FormShipDesigner : Form
+	{
+		private GameController controller;
+		
+		public FormShipDesigner()
+		{
+			InitializeComponent();
+		}
+		
+		public FormShipDesigner(GameController controller) : this()
+		{
+			this.controller = controller;
+		}
+		
+		protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+		{
+			if (keyData == Keys.Escape) 
+				this.Close();
+			return base.ProcessCmdKey(ref msg, keyData);
+		}
+	}
+}
