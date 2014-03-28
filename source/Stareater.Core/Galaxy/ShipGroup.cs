ec10f3dae272213b0c25005ff78f5003ecb74b71
Ivan Kravar??an <subchannel13@gmail.com>
2014-03-28 16:08:29 +0100

    Classes for describing stationary ships

---
+++ source/Stareater.Core/Galaxy/ShipGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Stareater.Ships;
+
+namespace Stareater.Galaxy
+{
+	class ShipGroup
+	{
+		public Design Design { get; private set; }
+		public long Quantity { get; set; }
+		
+		public ShipGroup(Design design, long quantity)
+		{
+			this.Design = design;
+			this.Quantity = quantity;
+		}
+	}
+}
---
+++ source/Stareater.Core/Galaxy/StationaryFleet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Stareater.GameData.Databases.Tables;
+using Stareater.Players;
+
+namespace Stareater.Galaxy
+{
+	class StationaryFleet
+	{
+		public Player Owner { get; private set; }
+		public StarData Location { get; private set; }
+		
+		public ShipGroupCollection Ships { get; private set; } //TODO(v0.5): make type
+		private object mission; //TODO(v0.5): make type
+		
+		public StationaryFleet(Player owner, StarData location)
+		{
+			this.Owner = owner;
+			this.Location = location;
+			
+			this.Ships = new ShipGroupCollection();
+		}
+	}
+}
