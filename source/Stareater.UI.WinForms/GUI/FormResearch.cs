118d486082b48bdbf6dcc170694f3b55592d6678
Ivan Kravar??an <subchannel13@gmail.com>
2014-04-30 15:58:16 +0200

    Less stuby research GUI

--- source/Stareater.UI.WinForms/GUI/FormResearch.cs
+++ source/Stareater.UI.WinForms/GUI/FormResearch.cs
@@ -22,7 +22,22 @@ namespace Stareater.GUI
 		public FormResearch(GameController controller) : this()
 		{
 			this.controller = controller;
-			this.topics = controller.DevelopmentTopics().ToList();
+			this.topics = controller.ResearchTopics().ToList();
+			
+			topicList.SuspendLayout();
+			
+			while (topicList.Controls.Count < topics.Count)
+				topicList.Controls.Add(new TechnologyItem());
+			while (topicList.Controls.Count > topics.Count)
+				topicList.Controls.RemoveAt(topicList.Controls.Count - 1);
+
+			for (int i = 0; i < topics.Count; i++)
+				(topicList.Controls[i] as TechnologyItem).SetData(topics[i]);
+			
+			topicList.ResumeLayout();
+			
+			Context context = SettingsWinforms.Get.Language["FormResearch"];
+			this.Text = context["FormTitle"].Text();
 		}
 		
 		protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
