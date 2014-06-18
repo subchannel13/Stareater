a59ccb0cd71147c6531e10937ab6e886126940f2
Ivan Kravar??an <subchannel13@gmail.com>
2014-06-18 15:59:29 +0200

    Beginnings of save game functionality

---
+++ source/Stareater.Core/Controllers/Data/SavedGameData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Stareater.Controllers.Data
+{
+	public class SavedGameData
+	{
+		
+		public int Turn { get; private set; }
+		public string Title { get; private set; }
+	}
+}
--- source/Stareater.UI.WinForms/GUI/FormSaveLoad.cs
+++ source/Stareater.UI.WinForms/GUI/FormSaveLoad.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using Stareater.Controllers;
 
 namespace Stareater.GUI
 {
 	public partial class FormSaveLoad : Form
 	{
+		private SavesController controller;
+		
 		public FormSaveLoad()
 		{
 			InitializeComponent();
+		}
+		
+		public FormSaveLoad(SavesController controller) : this()
+		{
+			this.controller = controller;
 		}
 		
 		protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
