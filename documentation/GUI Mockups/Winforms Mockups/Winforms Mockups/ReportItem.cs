addf5a9358e013a8c5e64c4786253d3c612b5282
Ivan Kravar??an <subchannel13@gmail.com>
2014-09-30 15:25:35 +0200

    Report GUI mockup

---
+++ documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/ReportItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Winforms_Mockups
+{
+	public partial class ReportItem : UserControl
+	{
+		public ReportItem()
+		{
+			InitializeComponent();
+		}
+	}
+}
