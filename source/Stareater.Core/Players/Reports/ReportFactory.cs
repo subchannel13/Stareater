097b5e4b33b6eada4fb4ec61aa3b5fdd277c8074
Ivan Kravar??an <Ivan Kravar??an@localhost>
2015-01-24 16:41:46 +0100

    Factory classes for reports and ship missions

---
+++ source/Stareater.Core/Players/Reports/ReportFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ikadn.Ikon.Types;
+using Stareater.Utils.Collections;
+
+namespace Stareater.Players.Reports
+{
+	static class ReportFactory
+	{
+		public static IReport Load(IkonComposite reportData, ObjectDeindexer deindexer)
+		{
+			if (reportData.Tag.Equals(TechnologyReport.SaveTag))
+				return TechnologyReport.Load(reportData, deindexer);
+
+			//TODO(later): add error handling
+			throw new NotImplementedException();
+		}
+	}
+}
