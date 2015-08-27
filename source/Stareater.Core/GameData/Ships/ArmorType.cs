bd89f24bf7e4a20315380f189fbbefee089e7615
Ivan Kravarščan <subchannel13@gmail.com>
2015-08-27 16:11:02 +0200

    ArmorType class

---
+++ source/Stareater.Core/GameData/Ships/ArmorType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Stareater.AppData.Expressions;
+
+namespace Stareater.GameData.Ships
+{
+	class ArmorType : AComponentType
+	{
+		private Formula armorFactor;
+		private Formula absorption;
+		private Formula absorptionMax;
+		
+		public ArmorType(string code, string nameCode, string descCode, string imagePath,
+		                 IEnumerable<Prerequisite> prerequisites, int maxLevel, 
+		                 Formula armorFactor, Formula absorption, Formula absorptionMax)
+			: base(code, nameCode, descCode, prerequisites, maxLevel)
+		{
+			this.armorFactor = armorFactor;
+			this.absorption = absorption;
+			this.absorptionMax = absorptionMax;
+		}
+	}
+}
