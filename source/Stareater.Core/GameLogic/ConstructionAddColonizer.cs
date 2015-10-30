6a839b2a5c730faf1a843da126bc5cc8080605fa
Ivan Kravarščan <subchannel13@gmail.com>
2015-10-30 16:08:33 +0100

    Colonizer specific building effect

---
+++ source/Stareater.Core/GameLogic/ConstructionAddColonizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Stareater.Galaxy;
+using Stareater.GameData;
+using Stareater.GameData.Databases;
+using Stareater.Ships;
+
+namespace Stareater.GameLogic
+{
+	public class ConstructionAddColonizer : IConstructionEffect
+	{
+		private Design colonizerDesign;
+		private ColonizationProject targetColony;
+		
+		public ConstructionAddColonizer(Design colonizerDesign, ColonizationProject targetColony)
+		{
+			this.colonizerDesign = colonizerDesign;
+			this.targetColony = targetColony;
+		}
+			
+		#region IConstructionEffect implementation
+		public void Apply(StatesDB states, AConstructionSite site, double quantity)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+		
+	}
+}
