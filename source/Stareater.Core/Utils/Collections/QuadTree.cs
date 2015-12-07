e49decb2e14aa9971c487994546b2387800a3c7d
Ivan Kravarščan <subchannel13@gmail.com>
2015-12-07 10:58:28 +0100

    Fixed a race condition while redrawing a map

--- source/Stareater.Core/Utils/Collections/QuadTree.cs
+++ source/Stareater.Core/Utils/Collections/QuadTree.cs
@@ -10,7 +10,7 @@ namespace Stareater.Utils.Collections
 		private const float MinSize = 1.0f / 8;
 
 		private QuadTreeNode<T> root;
-		private Dictionary<T, QuadTreeElement<T>> boundedElements = new Dictionary<T, QuadTreeElement<T>>();
+		private readonly Dictionary<T, QuadTreeElement<T>> boundedElements = new Dictionary<T, QuadTreeElement<T>>();
 
 		private Vector2D topRight;
 		private Vector2D bottomLeft;
@@ -33,51 +33,63 @@ namespace Stareater.Utils.Collections
 
 		public IEnumerable<T> Query(Vector2D center, Vector2D size)
 		{
-			return root.Query(center + size / 2, center - size / 2);
+			lock (boundedElements)
+				return root.Query(center + size / 2, center - size / 2);
 		}
 
 		public void Add(T newItem, Vector2D center, Vector2D size)
 		{
-			var boundedItem = new QuadTreeElement<T>(newItem, center + size / 2, center - size / 2);
-			boundedElements.Add(newItem, boundedItem);
-			
-			if (!root.Insert(boundedItem, this.minNodeSize)) {
-				var halfSize = (topRight - bottomLeft) / 2;
+			lock (boundedElements)
+			{
+				var boundedItem = new QuadTreeElement<T>(newItem, center + size / 2, center - size / 2);
+				boundedElements.Add(newItem, boundedItem);
 				
-				while(!Methods.IsRectEnveloped(this.topRight, this.bottomLeft, boundedItem.TopRight, boundedItem.BottomLeft)) {
-					this.topRight += halfSize;
-					this.bottomLeft -= halfSize;
-					halfSize *= 2;
+				if (!root.Insert(boundedItem, this.minNodeSize)) {
+					var halfSize = (topRight - bottomLeft) / 2;
+					
+					while(!Methods.IsRectEnveloped(this.topRight, this.bottomLeft, boundedItem.TopRight, boundedItem.BottomLeft)) {
+						this.topRight += halfSize;
+						this.bottomLeft -= halfSize;
+						halfSize *= 2;
+					}
+					
+					var oldRoot = this.root;
+					this.root = new QuadTreeNode<T>(topRight, bottomLeft);
+					this.root.Insert(boundedItem, this.minNodeSize);
+	
+					foreach (var oldItem in oldRoot.SubTreeContents)
+						root.Insert(oldItem, this.minNodeSize);
 				}
-				
-				var oldRoot = this.root;
-				this.root = new QuadTreeNode<T>(topRight, bottomLeft);
-				this.root.Insert(boundedItem, this.minNodeSize);
-
-				foreach (var oldItem in oldRoot.SubTreeContents)
-					root.Insert(oldItem, this.minNodeSize);
 			}
 		}
 
 		public void Clear()
 		{
-			root.Clear();
-			boundedElements.Clear();
+			lock (boundedElements)
+			{
+				root.Clear();
+				boundedElements.Clear();
+			}
 		}
 
 		public bool Remove(T item)
 		{
-			if (!boundedElements.ContainsKey(item))
-				return false;
+			lock (boundedElements)
+			{
+				if (!boundedElements.ContainsKey(item))
+					return false;
 
-			var boundedItem = boundedElements[item];
-			boundedElements.Remove(item);
-			return root.Remove(boundedItem);
+				var boundedItem = boundedElements[item];
+				boundedElements.Remove(item);
+				return root.Remove(boundedItem);
+			}
 		}
 
 		public IEnumerable<T> GetAll()
 		{
-			return boundedElements.Keys;
+			lock (boundedElements)
+				foreach(var element in boundedElements.Keys)
+					yield return element;
 		}
 	}
 }
