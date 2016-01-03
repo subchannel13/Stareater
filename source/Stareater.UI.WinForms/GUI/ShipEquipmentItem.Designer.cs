7e03ad68e725efc69cad2c85d7a73603767dce62
Ivan Kravarscan <subchannel13@gmail.com>
2016-01-03 15:49:30 +0100

    Ship equipment item, not yet on designer GUI

---
+++ source/Stareater.UI.WinForms/GUI/ShipEquipmentItem.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Stareater.GUI
+{
+	partial class ShipEquipmentItem
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.amountLabel = new System.Windows.Forms.Label();
+			this.thumbnail = new System.Windows.Forms.PictureBox();
+			this.nameLabel = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.thumbnail)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// amountLabel
+			// 
+			this.amountLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.amountLabel.Location = new System.Drawing.Point(0, 0);
+			this.amountLabel.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+			this.amountLabel.Name = "amountLabel";
+			this.amountLabel.Size = new System.Drawing.Size(50, 30);
+			this.amountLabel.TabIndex = 0;
+			this.amountLabel.Text = "x.xx X";
+			this.amountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// thumbnail
+			// 
+			this.thumbnail.Location = new System.Drawing.Point(53, 0);
+			this.thumbnail.Name = "thumbnail";
+			this.thumbnail.Size = new System.Drawing.Size(30, 30);
+			this.thumbnail.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.thumbnail.TabIndex = 1;
+			this.thumbnail.TabStop = false;
+			// 
+			// nameLabel
+			// 
+			this.nameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.nameLabel.Location = new System.Drawing.Point(86, 0);
+			this.nameLabel.Name = "nameLabel";
+			this.nameLabel.Size = new System.Drawing.Size(150, 30);
+			this.nameLabel.TabIndex = 2;
+			this.nameLabel.Text = "Name";
+			this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// ShipEquipmentItem
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(216)))), ((int)(((byte)(216)))));
+			this.Controls.Add(this.nameLabel);
+			this.Controls.Add(this.thumbnail);
+			this.Controls.Add(this.amountLabel);
+			this.Name = "ShipEquipmentItem";
+			this.Size = new System.Drawing.Size(375, 30);
+			((System.ComponentModel.ISupportInitialize)(this.thumbnail)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label amountLabel;
+		private System.Windows.Forms.PictureBox thumbnail;
+		private System.Windows.Forms.Label nameLabel;
+	}
+}
---
+++ source/Stareater.UI.WinForms/GUI/ShipEquipmentItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Stareater.GUI.ShipDesigns;
+using Stareater.AppData;
+
+namespace Stareater.GUI
+{
+	public partial class ShipEquipmentItem : UserControl
+	{
+		private double amount;
+		private IShipComponentChoice data;
+		
+		public ShipEquipmentItem()
+		{
+			InitializeComponent();
+		}
+
+		public IShipComponentChoice Data
+		{
+			get
+			{
+				return data;
+			}
+			set
+			{
+				this.data = value;
+
+				thumbnail.Image = ImageCache.Get[data.ImagePath];
+				nameLabel.Text = data.Name;
+			}
+		}
+
+		public double Amount
+		{
+			get
+			{
+				return amount;
+			}
+			set
+			{
+				this.amount = value;
+
+				amountLabel.Text = amount.ToString(); //TODO(v0.5) format
+			}
+		}
+	}
+}
