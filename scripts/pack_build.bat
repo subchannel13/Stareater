2b69319a0825dcb5af55ac67dcfc56a22f7b4570
Ivan Kravarščan <subchannel13@gmail.com>
2016-01-07 16:04:05 +0100

    Improved release packing and "works on my machine" fix

--- scripts/pack_build.bat
+++ scripts/pack_build.bat
@@ -8,4 +8,9 @@ call copy_game_to_build.bat
 
 rmdir /s /q ..\release\Stareater
 del /q ..\release\
-robocopy ../build/ ../release/Stareater/ /xd .svn /e
+robocopy ../build/ ../release/Stareater/
+robocopy ../build/data/ ../release/Stareater/data/ /e
+robocopy ../build/images/ ../release/Stareater/images/ /e
+robocopy ../build/languages/ ../release/Stareater/languages/ /e
+robocopy ../build/maps/ ../release/Stareater/maps/ /XF *.pdb
+robocopy ../build/players/ ../release/Stareater/players/ /XF *.pdb /XF *.txt
--- source/Stareater.UI.WinForms/Properties/AssemblyInfo.cs
+++ source/Stareater.UI.WinForms/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("Stareater WinForms UI")]
+[assembly: AssemblyTitle ("Stareater")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("Stareater WinForms UI")]
+[assembly: AssemblyProduct ("Stareater")]
 [assembly: AssemblyCopyright ("Copyright ©  2015")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
