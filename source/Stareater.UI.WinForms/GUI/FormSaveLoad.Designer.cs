a6fabe07033d3c64e99fe4729f7a2cf747c774e3
Ivan Kravarščan <subchannel13@gmail.com>
2016-06-17 09:17:43 +0200

    Fixed no scroll in save game list

--- source/Stareater.UI.WinForms/GUI/FormSaveLoad.Designer.cs
+++ source/Stareater.UI.WinForms/GUI/FormSaveLoad.Designer.cs
@@ -43,6 +43,7 @@ namespace Stareater.GUI
 			// 
 			// gameList
 			// 
+			this.gameList.AutoScroll = true;
 			this.gameList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
 									| System.Windows.Forms.AnchorStyles.Left) 
 									| System.Windows.Forms.AnchorStyles.Right)));
