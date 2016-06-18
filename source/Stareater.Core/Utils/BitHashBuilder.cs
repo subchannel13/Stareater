d60617e562966b26d53ee9cc2b50f16a7056f705
Ivan Kravarscan <subchannel13@gmail.com>
2016-06-18 16:18:09 +0200

    Fixed false positive duplicate design

--- source/Stareater.Core/Utils/BitHashBuilder.cs
+++ source/Stareater.Core/Utils/BitHashBuilder.cs
@@ -6,23 +6,23 @@ namespace Stareater.Utils
 	public class BitHashBuilder
 	{
 		private readonly List<uint> contents = new List<uint>();
-		private readonly List<uint> maxValues = new List<uint>();
 		private uint current = 0;
 		private uint factor = 1;
 		
 		public void Add(int value, int maxValue)
 		{
-			if (uint.MaxValue / (uint)maxValue < factor) {
+			uint factorStep = 2;
+			while (factorStep < (uint)maxValue)
+				factorStep *= 2;
+
+			if (uint.MaxValue / factorStep < factor)
+			{
 				factor = 1;
 				contents.Add(current);
 				current = 0;
 			}
 
 			current += (uint)value * factor;
-
-			uint factorStep = 2;
-			while (factorStep < (uint)maxValue)
-				factorStep *= 2;
 			factor *= factorStep;
 		}
 		
