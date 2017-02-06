d8d248548a39c69b6411be9c132542163a7e93e0
Ivan Kravarscan <subchannel13@gmail.com>
2017-02-06 21:08:21 +0100

    Polar coordinates in orbit shader

--- source/Stareater.UI.WinForms/shaders/OrbitFragment.glsl
+++ source/Stareater.UI.WinForms/shaders/OrbitFragment.glsl
@@ -4,13 +4,29 @@ uniform float minR;
 uniform float maxR;
 uniform vec4 color;
 
-in  vec2 orbitPositionFrag;
+in vec2 orbitPositionFrag;
 
 out vec4 outputF;
 
+#define M_PI 3.1415926535897932384626433832795
+
+vec2 cartToPolar(vec2 v)
+{
+	if (v.x == 0 && v.y == 0)
+		return vec2(0, 0);
+
+	float r = length(v);
+	float angle = abs(v.x) > abs(v.y) ?
+		v.x >= 0 ? asin(v.y / r) : M_PI - asin(v.y / r) :
+		acos(v.x / r) * sign(v.y);
+
+	return vec2(r, angle >= 0 ? angle : angle + 2 * M_PI);
+}
+
 void main()
 {
-   float r = length(orbitPositionFrag);
+   vec2 p = cartToPolar(orbitPositionFrag);
+   float r = p.x;
    
    if (r >= minR && r <= maxR)
       outputF = color;
