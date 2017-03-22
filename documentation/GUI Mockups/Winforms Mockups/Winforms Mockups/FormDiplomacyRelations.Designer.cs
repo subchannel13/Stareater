739ad2c8996680431e6b1a43ddeb1682f6d94eed
Ivan Kravarščan <subchannel13@gmail.com>
2017-03-22 15:42:28 +0100

    Diplomacy relations GUI mockup improvement

--- documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/FormDiplomacyRelations.Designer.cs
+++ documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/FormDiplomacyRelations.Designer.cs
@@ -14,9 +14,13 @@ namespace Winforms_Mockups
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private My_ListView.ControlListView controlListView1;
 		private Winforms_Mockups.RelationsPlayerInfo relationsPlayerInfo1;
 		private Winforms_Mockups.RelationsPlayerInfo relationsPlayerInfo2;
-		private System.Windows.Forms.Button button1;
+		private Winforms_Mockups.RelationsPlayerInfo relationsPlayerInfo3;
+		private Winforms_Mockups.RelationsPlayerInfo relationsPlayerInfo5;
+		private Winforms_Mockups.RelationsPlayerInfo relationsPlayerInfo4;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -39,29 +43,21 @@ namespace Winforms_Mockups
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.controlListView1 = new My_ListView.ControlListView();
 			this.relationsPlayerInfo1 = new Winforms_Mockups.RelationsPlayerInfo();
 			this.relationsPlayerInfo2 = new Winforms_Mockups.RelationsPlayerInfo();
-			this.button1 = new System.Windows.Forms.Button();
+			this.relationsPlayerInfo3 = new Winforms_Mockups.RelationsPlayerInfo();
+			this.relationsPlayerInfo4 = new Winforms_Mockups.RelationsPlayerInfo();
+			this.relationsPlayerInfo5 = new Winforms_Mockups.RelationsPlayerInfo();
+			this.controlListView1.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// relationsPlayerInfo1
-			// 
-			this.relationsPlayerInfo1.Location = new System.Drawing.Point(12, 12);
-			this.relationsPlayerInfo1.Name = "relationsPlayerInfo1";
-			this.relationsPlayerInfo1.Size = new System.Drawing.Size(150, 75);
-			this.relationsPlayerInfo1.TabIndex = 0;
-			// 
-			// relationsPlayerInfo2
-			// 
-			this.relationsPlayerInfo2.Location = new System.Drawing.Point(12, 93);
-			this.relationsPlayerInfo2.Name = "relationsPlayerInfo2";
-			this.relationsPlayerInfo2.Size = new System.Drawing.Size(150, 75);
-			this.relationsPlayerInfo2.TabIndex = 1;
 			// 
 			// button1
 			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.button1.Image = global::Winforms_Mockups.Properties.Resources.message;
-			this.button1.Location = new System.Drawing.Point(179, 207);
+			this.button1.Location = new System.Drawing.Point(402, 232);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(93, 43);
 			this.button1.TabIndex = 2;
@@ -69,16 +65,71 @@ namespace Winforms_Mockups
 			this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
 			this.button1.UseVisualStyleBackColor = true;
 			// 
+			// controlListView1
+			// 
+			this.controlListView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			| System.Windows.Forms.AnchorStyles.Left)));
+			this.controlListView1.AutoScroll = true;
+			this.controlListView1.Controls.Add(this.relationsPlayerInfo1);
+			this.controlListView1.Controls.Add(this.relationsPlayerInfo2);
+			this.controlListView1.Controls.Add(this.relationsPlayerInfo3);
+			this.controlListView1.Controls.Add(this.relationsPlayerInfo5);
+			this.controlListView1.Controls.Add(this.relationsPlayerInfo4);
+			this.controlListView1.Location = new System.Drawing.Point(12, 12);
+			this.controlListView1.Name = "controlListView1";
+			this.controlListView1.Size = new System.Drawing.Size(350, 263);
+			this.controlListView1.TabIndex = 3;
+			// 
+			// relationsPlayerInfo1
+			// 
+			this.relationsPlayerInfo1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.relationsPlayerInfo1.Location = new System.Drawing.Point(3, 3);
+			this.relationsPlayerInfo1.Name = "relationsPlayerInfo1";
+			this.relationsPlayerInfo1.Size = new System.Drawing.Size(160, 120);
+			this.relationsPlayerInfo1.TabIndex = 0;
+			// 
+			// relationsPlayerInfo2
+			// 
+			this.relationsPlayerInfo2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.relationsPlayerInfo2.Location = new System.Drawing.Point(169, 3);
+			this.relationsPlayerInfo2.Name = "relationsPlayerInfo2";
+			this.relationsPlayerInfo2.Size = new System.Drawing.Size(160, 120);
+			this.relationsPlayerInfo2.TabIndex = 1;
+			// 
+			// relationsPlayerInfo3
+			// 
+			this.relationsPlayerInfo3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.relationsPlayerInfo3.Location = new System.Drawing.Point(3, 129);
+			this.relationsPlayerInfo3.Name = "relationsPlayerInfo3";
+			this.relationsPlayerInfo3.Size = new System.Drawing.Size(160, 120);
+			this.relationsPlayerInfo3.TabIndex = 2;
+			// 
+			// relationsPlayerInfo4
+			// 
+			this.relationsPlayerInfo4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.relationsPlayerInfo4.Location = new System.Drawing.Point(3, 255);
+			this.relationsPlayerInfo4.Name = "relationsPlayerInfo4";
+			this.relationsPlayerInfo4.Size = new System.Drawing.Size(160, 120);
+			this.relationsPlayerInfo4.TabIndex = 3;
+			// 
+			// relationsPlayerInfo5
+			// 
+			this.relationsPlayerInfo5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.relationsPlayerInfo5.Location = new System.Drawing.Point(169, 129);
+			this.relationsPlayerInfo5.Name = "relationsPlayerInfo5";
+			this.relationsPlayerInfo5.Size = new System.Drawing.Size(160, 120);
+			this.relationsPlayerInfo5.TabIndex = 4;
+			// 
 			// FormDiplomacyRelations
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(507, 287);
+			this.Controls.Add(this.controlListView1);
 			this.Controls.Add(this.button1);
-			this.Controls.Add(this.relationsPlayerInfo2);
-			this.Controls.Add(this.relationsPlayerInfo1);
 			this.Name = "FormDiplomacyRelations";
 			this.Text = "FormDiplomacyRelations";
+			this.controlListView1.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
--- documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/RelationsPlayerInfo.Designer.cs
+++ documentation/GUI Mockups/Winforms Mockups/Winforms Mockups/RelationsPlayerInfo.Designer.cs
@@ -18,6 +18,10 @@ namespace Winforms_Mockups
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.Label label4;
 		
 		/// <summary>
 		/// Disposes resources used by the control.
@@ -44,7 +48,14 @@ namespace Winforms_Mockups
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.label4 = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -69,7 +80,7 @@ namespace Winforms_Mockups
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(42, 43);
+			this.label2.Location = new System.Drawing.Point(42, 42);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(50, 13);
 			this.label2.TabIndex = 4;
@@ -78,23 +89,70 @@ namespace Winforms_Mockups
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(42, 56);
+			this.label3.Location = new System.Drawing.Point(42, 68);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(64, 13);
 			this.label3.TabIndex = 5;
 			this.label3.Text = "Trade treaty";
 			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = global::Winforms_Mockups.Properties.Resources.hydroponic_farms;
+			this.pictureBox2.Location = new System.Drawing.Point(16, 42);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 6;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = global::Winforms_Mockups.Properties.Resources.hydroponic_farms;
+			this.pictureBox3.Location = new System.Drawing.Point(16, 68);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(20, 20);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 7;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Image = global::Winforms_Mockups.Properties.Resources.message;
+			this.pictureBox4.Location = new System.Drawing.Point(16, 94);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(20, 20);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox4.TabIndex = 9;
+			this.pictureBox4.TabStop = false;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(42, 94);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(106, 13);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Requested audience";
+			// 
 			// RelationsPlayerInfo
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox1);
 			this.Name = "RelationsPlayerInfo";
-			this.Size = new System.Drawing.Size(150, 75);
+			this.Size = new System.Drawing.Size(160, 120);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
