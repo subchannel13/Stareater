a71c73e838eb8f1530215aa1382fbcbbbf04af3a
Ivan Kravarščan <subchannel13@gmail.com>
2017-09-07 10:39:08 +0200

    Save preview image

--- source/Stareater.UI.WinForms/GUI/SavePreviewGenerator.cs
+++ source/Stareater.UI.WinForms/GUI/SavePreviewGenerator.cs
@@ -1,11 +1,20 @@
 ﻿using Ikadn.Ikon.Types;
 using Stareater.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 
 namespace Stareater.GUI
 {
 	class SavePreviewGenerator
 	{
+		public const string Tag = "WinformsPreview";
+		private const string StarTag = "Star";
+		private const string StarsKey = "Stars";
+		private const string TurnKey = "turn";
+		private const string ColorKey = "color";
+
 		private GameController controller;
 
 		public SavePreviewGenerator(GameController controller)
@@ -15,8 +24,51 @@ namespace Stareater.GUI
 
 		public IkonBaseObject Make()
 		{
-			//TODO(v0.7) add star map data
-			return new IkonInteger(this.controller.LocalHumanPlayers().First().Turn);
+			var player = this.controller.LocalHumanPlayers().First();
+			var starData = new IkonArray();
+			foreach(var star in player.Stars)
+			{
+				var data = new IkonComposite(StarTag);
+				data.Add("x", new IkonFloat(star.Position.X));
+				data.Add("y", new IkonFloat(star.Position.Y));
+				data.Add(ColorKey, new IkonArray(
+					new []
+					{
+						new IkonInteger(star.Color.R),
+						new IkonInteger(star.Color.G),
+						new IkonInteger(star.Color.B)
+					}
+				));
+				starData.Add(data);
+			}
+
+			var previewData = new IkonComposite(Tag);
+			previewData.Add(TurnKey, new IkonInteger(player.Turn));
+			previewData.Add(StarsKey, starData);
+
+			return previewData;
 		}
+
+		public static int TurnOf(IkonBaseObject previewData)
+		{
+			return previewData.To<IkonComposite>()[TurnKey].To<int>();
+        }
+
+		public static IEnumerable<Tuple<PointF, Color>> StarsOf(IkonBaseObject previewData)
+		{
+			var starData = previewData.To<IkonComposite>()[StarsKey].To<IkonArray>();
+
+			foreach (var item in starData.Select(x => x.To<IkonComposite>()))
+			{
+				var colorData = item[ColorKey].To<int[]>();
+				yield return new Tuple<PointF, Color>(
+					new PointF(
+						item["x"].To<float>(),
+						-item["y"].To<float>()
+					),
+					Color.FromArgb(colorData[0], colorData[1], colorData[2])
+				);
+			}
+        }
 	}
 }
--- source/Stareater.UI.WinForms/GUI/SavedGameItemView.cs
+++ source/Stareater.UI.WinForms/GUI/SavedGameItemView.cs
@@ -5,12 +5,16 @@ using Stareater.Controllers.Views;
 using Stareater.Localization;
 using Stareater.Properties;
 using Stareater.Utils.Collections;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Stareater.GUI
 {
 	public partial class SavedGameItemView : UserControl
 	{
 		private SavedGameInfo gameData;
+		private Image previewImage = null;
 		
 		public SavedGameItemView()
 		{
@@ -27,13 +31,22 @@ namespace Stareater.GUI
 			{
 				this.gameData = value;
 				
-				Context context = LocalizationManifest.Get.CurrentLanguage[FormSaveLoad.LanguageContext];
+				if (this.previewImage != null)
+				{
+					this.previewImage.Dispose();
+					this.previewImage = null;
+                }
+
+				var context = LocalizationManifest.Get.CurrentLanguage[FormSaveLoad.LanguageContext];
 				
 				if (value != null)
 				{
 					this.gameName.Text = gameData.Title;
-                    this.turnText.Text = context["Turn"].Text(null, new TextVar("turn", gameData.PreviewData.To<int>().ToString()).Get);
-				}
+                    this.turnText.Text = context["Turn"].Text(null, new TextVar("turn", SavePreviewGenerator.TurnOf(gameData.PreviewData).ToString()).Get);
+
+					this.previewImage = this.drawPreview(SavePreviewGenerator.StarsOf(gameData.PreviewData).ToList());
+					this.preview.Image = this.previewImage;
+                }
 				else
 				{
 					this.preview.Image = Resources.newSave;
@@ -73,5 +86,38 @@ namespace Stareater.GUI
 		{
 			this.InvokeOnClick(this, e);
 		}
+
+		private Image drawPreview(IEnumerable<Tuple<PointF, Color>> stars)
+		{
+			const int padding = 3;
+			const float dotSize = 1.5f;
+
+			var minX = stars.Select(p => p.Item1.X).Min();
+			var minY = stars.Select(p => p.Item1.Y).Min();
+			var maxX = stars.Select(p => p.Item1.X).Max();
+			var maxY = stars.Select(p => p.Item1.Y).Max();
+
+			var scaleX = (this.preview.Width - 2 * padding) / (maxX - minX);
+			var scaleY = (this.preview.Height - 2 * padding) / (maxY - minY);
+
+			var image = new Bitmap(this.preview.Width, this.preview.Height);
+			using (var g = Graphics.FromImage(image))
+			{
+				g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+				g.Clear(Color.Black);
+
+				foreach (var star in stars)
+					using (var starColor = new SolidBrush(star.Item2))
+						g.FillEllipse(
+							starColor,
+							(star.Item1.X - minX) * scaleX + padding,
+							(star.Item1.Y - minY) * scaleY + padding,
+							dotSize,
+							dotSize
+						);
+			}
+
+			return image;
+        }
 	}
 }
