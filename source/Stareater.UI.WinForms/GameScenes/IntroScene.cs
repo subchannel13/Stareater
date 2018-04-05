25a6a01887e7e1650a84ae365030a7a02ea80711
Ivan Kravarščan <subchannel13@gmail.com>
2018-04-05 12:47:46 +0200

    Intro scene random star colors and sizes

--- source/Stareater.UI.WinForms/GameScenes/IntroScene.cs
+++ source/Stareater.UI.WinForms/GameScenes/IntroScene.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using OpenTK;
 using Stareater.GLData;
 using System.Drawing;
-using Stareater.GLData.SpriteShader;
 using Stareater.GraphicsEngine.Animators;
 
 namespace Stareater.GameScenes
@@ -16,6 +15,7 @@ namespace Stareater.GameScenes
 		private const int StarfieldSeed = 101;
 		private const float StarDisplacement = 0.7f;
 		private const float StarSize = 0.03f;
+		private const float StarSizeVariance = 0.4f;
 
 		private readonly Vector2 StareaterPathDirection = new Vector2(-2, -1).Normalized();
 		private readonly Vector2 StareaterPathOffset = new Vector2(0, 0.1f);
@@ -32,7 +32,7 @@ namespace Stareater.GameScenes
 		private const float StarColorZ = 3 / Layers;
 		private const float StarSaturationZ = 2 / Layers;
 
-		private SceneObject staticStarSprites = null;
+		private IEnumerable<SceneObject> staticStarSprites = null;
 		private IEnumerable<SceneObject> fadingStarSprites = null;
 		private Action timeoutCallback;
 
@@ -61,8 +61,8 @@ namespace Stareater.GameScenes
 		public override void Activate()
 		{
 			var random = new Random(StarfieldSeed);
-			var staticStars = new List<Vector2>();
-			var fadingStars = new List<Vector2>();
+			var staticStars = new List<Star>();
+			var fadingStars = new List<Star>();
 			var pathNormal = StareaterPathDirection.PerpendicularLeft;
 
 			for (int y = -StarfieldRadius; y <= StarfieldRadius; y++)
@@ -72,47 +72,43 @@ namespace Stareater.GameScenes
 						(x + (float)(random.NextDouble() - 0.5) * StarDisplacement) / StarfieldRadius / 2,
 						(y + (float)(random.NextDouble() - 0.5) * StarDisplacement) / StarfieldRadius / 2
 					);
+					var star = new Star(
+						position,
+						starColor(random),
+						(float)random.NextDouble() * StarSizeVariance + 1 - StarSizeVariance
+					);
 
-                    if (Math.Abs(Vector2.Dot(pathNormal, position - StareaterPathOffset)) < 0.1 && PathPhase(position) < StareaterDistance)
-						fadingStars.Add(position);
+                    if (Math.Abs(Vector2.Dot(pathNormal, position - StareaterPathOffset)) < 0.1 && pathPhase(position) < StareaterDistance)
+						fadingStars.Add(star);
 					else
-						staticStars.Add(position);
+						staticStars.Add(star);
 				}
 
 			this.UpdateScene(
 				ref this.staticStarSprites,
-				new SceneObjectBuilder().
-					StartSprite(StarColorZ, GalaxyTextures.Get.StarColor.Id, Color.Yellow).
-					AddVertices(
-						staticStars.SelectMany(position => SpriteHelpers.TexturedRectVertexData(
-							position,
-							StarSize,
-							StarSize,
-							GalaxyTextures.Get.StarColor
-					))).
+				staticStars.Select(star => new SceneObjectBuilder().
+					StartSimpleSprite(StarColorZ, GalaxyTextures.Get.StarColor, star.Color).
+					Scale(star.Size * StarSize).
+					Translate(star.Position).
 
-					StartSprite(StarSaturationZ, GalaxyTextures.Get.StarGlow.Id, Color.White).
-					AddVertices(
-						staticStars.SelectMany(position => SpriteHelpers.TexturedRectVertexData(
-							position,
-							StarSize,
-							StarSize,
-							GalaxyTextures.Get.StarGlow
-					))).
+					StartSimpleSprite(StarSaturationZ, GalaxyTextures.Get.StarGlow, Color.White).
+					Scale(star.Size * StarSize).
+					Translate(star.Position).
 
 					Build()
+				).ToList()
 			);
 
 			this.UpdateScene(
 				ref this.fadingStarSprites,
-				fadingStars.OrderBy(p => PathPhase(p)).Select((position, i) => new SceneObjectBuilder().
-					StartSimpleSprite(StarColorZ, GalaxyTextures.Get.StarColor, Color.Yellow).
-					Scale(StarSize).
-					Translate(position).
+				fadingStars.OrderBy(star => pathPhase(star.Position)).Select((star, i) => new SceneObjectBuilder().
+					StartSimpleSprite(StarColorZ, GalaxyTextures.Get.StarColor, star.Color).
+					Scale(star.Size * StarSize).
+					Translate(star.Position).
 
 					StartSimpleSprite(StarSaturationZ, GalaxyTextures.Get.StarGlow, Color.White).
-					Scale(StarSize).
-					Translate(position).
+					Scale(star.Size * StarSize).
+					Translate(star.Position).
 
 					Build(polygons => new AnimationSequence(
 						new AnimationDelay(i * FadeDelayIncrement + FadeDelay),
@@ -125,9 +121,35 @@ namespace Stareater.GameScenes
 			);
 		}
 
-		private float PathPhase(Vector2 position)
+		private float pathPhase(Vector2 position)
 		{
 			return Vector2.Dot(StareaterPathDirection, position - StareaterPathOffset);
         }
-    }
+
+		private Color starColor(Random random)
+		{
+			var roll = random.NextDouble();
+			
+			if (roll < 0.6)
+				return Color.FromArgb(255, 32, 32);
+			if (roll < 0.8)
+				return Color.FromArgb(255, 224, 0);
+
+			return Color.FromArgb(0, 160, 255);
+		}
+
+		private struct Star
+		{
+			public Vector2 Position;
+			public Color Color;
+			public float Size;
+
+			public Star(Vector2 position, Color color, float size)
+			{
+				this.Position = position;
+				this.Color = color;
+				this.Size = size;
+			}
+		}
+	}
 }
