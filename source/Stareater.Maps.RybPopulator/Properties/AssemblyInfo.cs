194a2cff428efeb5f0ac5f4ad6054e01d9e666f0
Ivan Kravarščan <subchannel13@gmail.com>
2018-06-01 16:07:02 +0200

    Increased version

--- source/Stareater.Maps.RybPopulator/Properties/AssemblyInfo.cs
+++ source/Stareater.Maps.RybPopulator/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("Stareater.Maps.RybPopulator")]
-[assembly: AssemblyCopyright ("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("0.6.0.0")]
-[assembly: AssemblyFileVersion ("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
